488051b2f37021dbcb33fde74cb501900fdab299
Prabesh Aryal <mailforprabesh@gmail.com>
2026-07-19 13:43:23 +0545

    updated

--- javascript/function.js
+++ javascript/function.js
@@ -1,4 +1,4 @@
-//function 
+/*//function 
 //function is a block of code which can be reused multiple time
 function hello(){
     console.log("hello world")
@@ -12,15 +12,84 @@ function sum (a,b){
 console.log(sum(12,12))
 
 
-
-
-//function
-function myname(){
-    console.log("hello world")
+function add(x,y,c=10){
+    return x+y+c
 }
-myname()
+console.log(add(12,12,12))
 
-let obj ={"name":"bijay",age:27}
-console.log(typeof(obj))
+//arrow function (Es6 module)
+const Action = () => { //arrow function bhayera => deko
+    console.log("this is arrow function")
+}
+
+const val =(messi,ronaldo)=> {
+    return messi>ronaldo
+}
+console.log(val(10,7))
+
+//closure function (euta function bhitra arko function chalnu)
+
+function college(){
+    return "bsc csit"
+}
+
+const inne = () =>{
+    return college()
+}
+
+const outerFunc = () =>{
+    return inner()
+}
+
+console.log(outerFunc())
+
+
+
+
+// call back function (it returns the value)
+//setTimeout(() =>{console.log("out out out...")},3000)//1000 matlab 1 sec, 3000 matlab 3 sec paxi display hunxa
+//setInterval(()=>{console.log("hello")},2000)
+//const out = setInterval(()=>{console.log("hello")},2000)
+//clearInterval(out)*/
+
+//login form
+const login =(email,password)=>{
+    const adminEmail = "bj@gmail.com"
+    const adminPassword = "12345"
+    if (email == adminEmail && password == adminPassword){
+        return"login Successfull"
+    }
+    else{
+        return"login Failed"
+    }
+}
+console.log(login("bj@gmail.com","1234"))
+
+
+
+const register = () =>{
+    const username= prompt("Enter your name: ")
+    const email= prompt("Enter your email: ")
+    const password= prompt("Enter your password: ")
+    const confirmPassword = prompt("Confirm your password: ")
+
+    if(password == confirmPassword){
+        return "Password is correct"
+    }else{
+        return "Password not matched"
+    }
+
+    console.log({username, email, password, confirmPassword})
+}
+
+console.log(register())
+
+
+/*create a bank function inside it  you have paramter bank_name, bank_acc_no , bankBalanace
+
+
+- get balanace ()
+- withdarw()
+- deposit()*/
 
 
